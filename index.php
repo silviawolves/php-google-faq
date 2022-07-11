050202657656f80483b44daadec5b66476b1b7f3
silviawolves <silviacanepa.w@gmail.com>
2022-07-11 15:16:57 +0200

    impaginazione inutile

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
 <?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Privacy e Termini</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    
+    <nav class="border-bottom">
+
+        <!--INTESTAZIONE CON LOGO E TENDINA UTENTE-->
+        <div class="container pt-4 pb-2">
+            <div class="d-flex justify-content-between align-items-center">
+
+                <!--PARTE SINISTRA-->
+                <div class="d-flex">
+                    <img src="img/google_logo.png" alt="logo google">
+                    <h4 class="opacity-75 fw-normal ms-1">Privacy e Termini</h4>
+                </div>
+
+                <!--PARTE DESTRA-->
+                <div class="opacity-75 d-flex align-items-center">
+                    <i class="fa-solid fa-ellipsis-vertical"></i><i class="fa-solid fa-ellipsis-vertical"></i><i class="fa-solid fa-ellipsis-vertical"></i>
+                    <div class="user-icon"><i class="fa-solid fa-user-large"></i></div>
+                </div>
+            </div>
+
+            <div class="link-navigazione">
+                <a href="#">Introduzione</a>
+                <a href="#">Norme sulla privacy</a>
+                <a href="#">Termini di servizio</a>
+                <a href="#">Tecnologie</a>
+                <a href="#">Domande frequenti</a>
+            </div>
+        </div>
+
+    </nav>
+    
+</body>
+</html>

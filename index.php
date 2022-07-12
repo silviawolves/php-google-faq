9d2bf63541b18bd47717314dfdcde7a36da712b8
silviawolves <silviacanepa.w@gmail.com>
2022-07-12 09:24:27 +0200

    finezze grafiche

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $listaFaq = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Google Privacy e Termini</title>
+    <title>Domande Frequenti - Privacy e Termini</title>
 
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -99,7 +99,7 @@ $listaFaq = [
 
     </header>
 
-    <main class="pt-5 pb-4">
+    <main>
         <div class="container">
 
         <?php foreach ($listaFaq as $domanda) : ?>
